0f0fc21aeeade6c09310e2b5ef160d575759302c
PatriziaBozza <patrizia_bozza@hotmail.com>
2018-11-11 17:41:34 +0000

    Adding comment to shuffleArray function

--- src/js/MemoryGame.js
+++ src/js/MemoryGame.js
@@ -127,6 +127,7 @@ function resetGameBoard() {
     initializeCards();
 }
 
+// Algorithm to shuffle cards copied from Stack Overflow.
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
